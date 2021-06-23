cc213dca1841cb7b07013e708ea3aa3e5c374c41
Alexandre Viana Pereira <alexandrevianna511@gmail.com>
2021-06-23 15:27:14 -0400

    Ta funcionando
    Tela de login, tela splash e configurações

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_style.dart';
+import 'package:payflow/shared/themes/widgets/social_login/social_login_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,27 +14,57 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery
-        .of(context)
-        .size; //pega o tamanho da tela
+    final size = MediaQuery.of(context).size; //pega o tamanho da tela
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Container(
         width: size.width,
         height: size.height,
         child: Stack(
-        children: [
-        Container(
-        width: size.width,
-        height: size.height * 0.36, //altura da tela
-        color: AppColors.primary,
-      ),
-      Positioned(top:40, left: 0, right: 0,
-          child:
-      Image.asset(AppImages.person, width: 208, height: 373,
-      ))
-        ],
-      ),
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.36, //altura da tela
+              color: AppColors.primary,
+            ),
+            Positioned(
+                top: 40,
+                left: 0,
+                right: 0,
+                child: Image.asset(
+                  AppImages.person,
+                  width: 208,
+                  height: 280,
+                )),
+            Positioned(
+                bottom: size.height * 0.05,
+                left: 0,
+                right: 0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset(AppImages.logomini),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 2.5, left: 70, right: 70),
+                      child: Text("Organize seu boletos em um só lugar",
+                          textAlign: TextAlign.center,
+                          style: TextStyles.titleHome),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 40, left: 40, right: 40),
+                      child: SocialLoginButton(
+                        onTap: () {
+                          print("pegou");
+                        },
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
--- lib/shared/themes/app_text_style.dart
+++ lib/shared/themes/app_text_style.dart
@@ -2,6 +2,7 @@ import  'package:flutter/material.dart';
 import  'package:google_fonts/google_fonts.dart';
 import  'package:payflow/shared/themes/app_colors.dart';
 
+
 class  TextStyles {
   static  final titleHome = GoogleFonts.lexendDeca(
     fontSize: 32,
